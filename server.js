d0419e1a0f05361cdb46c1e82ec8b8d76ae7c831
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-03-10 23:09:53 +0600

    create server.js , app.js , errorHandler.js for better optimized code

---
+++ server.js
@@ -0,0 +1,7 @@
+import { v2 as cloudinary } from "cloudinary";
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+    api_key: process.env.CLOUDINARY_CLIENT_API,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
