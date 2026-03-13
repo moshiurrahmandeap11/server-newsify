022a331743041aefc893233e2af1036c2506d906
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-03-13 18:40:09 +0600

    all user route complete

--- routes/usersRoute/authRoutes.js
+++ routes/usersRoute/authRoutes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { get_user, log_out, login, register } from "../../controllers/authControllers.js";
+import { forgot_password, get_user, log_out, login, register, reset_password, update_password, updateProfile } from "../../controllers/authControllers.js";
+import { isAuthenticated } from "../../middleware/auth_middleware.js";
 
 const router = Router();
 
 router.post("/register", register)
 router.post("/login", login)
-router.get("/me", get_user)
-router.get("/logout", log_out)
+router.get("/me", isAuthenticated, get_user)
+router.get("/logout", isAuthenticated, log_out)
+router.post("/forgot-password", forgot_password)
+router.patch("/reset-password", reset_password)
+router.patch("/update-password", isAuthenticated, update_password)
+router.put("/update-profile", isAuthenticated, updateProfile);
 
 export default router;
